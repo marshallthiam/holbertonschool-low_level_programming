12d805be040f3143b20743fd8f2559574f71aaaf
marshallthiam <marshallthiam@holbertonschool-low_level_programmingl.com.com>
2025-05-10 13:18:15 +0000

    The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
-/*
- * File: 3-print_numbers.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9.
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 void print_numbers(void)
 {
-	int num;
+	int i;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
 
 	_putchar('\n');
 }
