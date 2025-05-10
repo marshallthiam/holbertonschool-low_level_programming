0524883082070dccd8452d2af2e38cc39dfa2119
marshallthiam <marshallthiam@holbertonschool-low_level_programmingl.com.com>
2025-05-10 12:53:46 +0000

    main..h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,25 +1,17 @@
-#ifndef main_H
-#define main_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
-
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
