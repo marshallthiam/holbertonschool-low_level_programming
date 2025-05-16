57186e21330b38ca10214b04f16fe41332c8d2ac
marshallthiam <marshallthiam@holbertonschool-low_level_programmingl.com>
2025-05-16 11:45:00 +0000

    I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
+#include <stdio.h> 
 /**
  * _puts - prints a string, followed by a new line,
  * @str: pointer to the string to print
  * Return: void
  */
-
 void _puts(char *str)
 {
 	int i = 0;
