3429721a0ffaa57a7714e7c0fba7904cfe2d73e0
marshallthiam <marshallthiam@holbertonschool-low_level_programmingl.com>
2025-05-17 00:04:28 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
  * *_strcat - function commute srtings
  * @dest: param pointer to a char
  * @src: param pointer to a char
  * Return: return value of dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-
 	i = 0;
 	j = 0;
-
 	while (dest[i] != '\0')
 	{
 		i++;
