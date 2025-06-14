799ad7d6d051b641e8cadcd587977cdbc576618d
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-14 20:24:16 +0000

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+""" Module that contains a function that reads from a file """
+
+
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its contents to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
