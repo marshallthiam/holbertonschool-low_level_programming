0d10c7b184a05db314f31d92d6dcf8cf73616b53
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-21 01:00:31 +0000

    List length

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,33 +1,26 @@
- /*
- * File: 0-print_list.c
- * Auth: Brennan D Baraban
- */
-
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the first node of the list
  *
- * Return: The number of nodes in h.
+ * Return: the number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+    size_t count = 0;
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+    while (h)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
+        count++;
+        h = h->next;
+    }
 
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
+    return (count);
 }
