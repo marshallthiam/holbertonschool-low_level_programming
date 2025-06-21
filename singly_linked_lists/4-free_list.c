acbf6174885a26ffa604dea492cb7961c5c2f994
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-21 13:41:34 +0000

    Free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,20 +1,24 @@
+/*
+ * File: 4-free_list.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "lists.h"
 #include <stdlib.h>
 
-typedef struct list_s
-{
-    char *str;
-    struct list_s *next;
-} list_t;
-
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
 void free_list(list_t *head)
 {
-    list_t *tmp;
+	list_t *tmp;
 
-    while (head)
-    {
-        tmp = head->next;
-        free(head->str);  // Free the string inside the node
-        free(head);       // Free the node itself
-        head = tmp;
-    }
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
