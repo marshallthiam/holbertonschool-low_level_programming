d2e103a14628881ca7282404c0dece4182e0fa90
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-21 00:30:40 +0000

    Print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,35 +1,33 @@
+ /*
+ * File: 0-print_list.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "lists.h"
 #include <stdio.h>
-#include <stddef.h>  // for size_t
 
 /**
- * struct list_s - singly linked list
- * @str: string - malloc'd string
- * @next: points to the next node
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
  */
-typedef struct list_s
-{
-    char *str;
-    struct list_s *next;
-} list_t;
 
-/**
- * print_list - prints all elements of a list_t list
- * @h: pointer to the first node of the list
- * Return: number of nodes
- */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%lu] %s\n", strlen(h->str), h->str);
-        count++;
-        h = h->next;
-    }
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-    return count;
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
