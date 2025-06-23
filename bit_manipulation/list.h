cf24d1de78266b60edfaad7096c84e9fba33ec52
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-23 21:16:46 +0000

    list.h

---
+++ bit_manipulation/list.h
@@ -0,0 +1,30 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+
+/* Converts a binary string to an unsigned int */
+unsigned int binary_to_uint(const char *b);
+
+/* Prints the binary representation of a number */
+void print_binary(unsigned long int n);
+
+/* Sets the value of a bit to 1 at a given index */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/* Sets the value of a bit to 0 at a given index */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/* Returns the value of a bit at a given index */
+int get_bit(unsigned long int n, unsigned int index);
+
+/* Returns the number of bits needed to flip to get from one number to another */
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+/* Checks the endianness of the machine */
+int get_endianness(void);
+
+#endif /* LISTS_H */
