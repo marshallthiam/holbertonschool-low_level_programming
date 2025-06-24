65b04133f5b4f2e20016e508e4ef8b09aa947248
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-24 11:57:27 +0000

    list.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -23,4 +23,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);  /* <= Ajoute ceci */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n);
 #endif
