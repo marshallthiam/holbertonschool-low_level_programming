224229eb58575ce56c30bbd8fbe57f582124fef0
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-24 11:32:25 +0000

    list.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -21,5 +21,5 @@ size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 size_t dlistint_len(const dlistint_t *h);
-
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 #endif
