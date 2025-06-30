614aee8a14e351c3b85d4e8e21db463ba6d87b02
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-30 10:55:43 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-#include <sys/types.h>
+int create_file(const char *filename, char *text_content);
 
-ssize_t read_textfile(const char *filename, size_t letters);
-
-#endif
+#endif /* MAIN_H */
